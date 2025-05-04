4fcd1f4d2751b2afb0bd5d39d4368bfcaab046ba
Dany Mitte <57202696+DMitte@users.noreply.github.com>
2025-05-04 18:10:48 -0500

    hecho el agregar editar y eliminar usuarios y el listar los usuarios por pagina

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,8 +1,27 @@
-import { Router } from 'express';
-import { getAllUsers } from '../controllers/userController';
+import { response, Router } from 'express';
+import { getAllUsers, aggUser, deletedUser, editedUser, getUserByIdC } from '../controllers/userController';
 
 const router = Router();
 
-router.get('/', getAllUsers);
+// Define routes
+router.get('/', getAllUsers); 
+
+router.get('/:id', (req, res) => {
+    getUserByIdC(req, res);
+});
+
+router.post('/agg', (req, res) => {
+    aggUser(req, res);
+}); 
+
+router.delete('/:id', (req, res) => {
+    deletedUser(req, res);
+});
+
+router.put('/:id', (req, res) => {
+    editedUser(req, res);
+});
+
+
 
 export default router;
