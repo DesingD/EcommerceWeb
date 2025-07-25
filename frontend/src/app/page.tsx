68e24f643baadec35627664235a11f6016a87a48
Dany Mitte <57202696+DMitte@users.noreply.github.com>
2025-07-25 00:13:50 -0500

    footer and header add

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
+import Header from "@/components/index/Header";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
+      <Header />
       <Footer />
     </div>
   );
