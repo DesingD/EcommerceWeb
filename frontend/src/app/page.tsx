7709cd5ff850825e12651189957c0ee9c1c8f2f3
Dany Mitte <57202696+DMitte@users.noreply.github.com>
2025-07-26 23:34:50 -0500

    categorias add index

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 import Header from "@/components/index/Header";
+import Categories from "@/components/index/Categories";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
       <Header />
+      <Categories />
       <Footer />
     </div>
   );
